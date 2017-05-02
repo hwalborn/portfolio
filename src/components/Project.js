bd306e9158858a2f8ef4039a8532780aae22786f
hwalborn <hwalborn@gmail.com>
2017-05-01 22:16:04 -0400

    worked on project display

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Parallax } from 'react-parallax'
 
-const Project = ({ title, img, description }) => {
+const handleClick = (e, id) =>{
+  e.preventDefault()
+  document.querySelector(`.project-description-${id}`).style.display = "block"
+  document.querySelector(`.par-${id}`).style.display = "none"
+}
+
+const Project = ({ id, title, img, description }) => {
+  let customDescription=`project-description-${id}`
+  let customId = `par-${id}`
   return (
     <div className="project-container">
-      <h1 className="about-title">{title}</h1>
-      <Parallax bgImage={img} strength={100} bgWidth="68%">
+      <a href='#' onClick={(e) => handleClick(e, id)}><h1 className="project-title">{title}</h1></a>
+      <Parallax className={customId} bgImage={img} strength={100} bgWidth="68%">
         <div style={{height: 400}}>
         </div>
       </Parallax>
-      <p className="about-content">{description}</p>
+      <p className={customDescription}>{description}</p>
     </div>
   )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,9 +4,9 @@ import { projectData } from '../data/projects.js'
 
 const Projects = () => {
   let show = projectData.map((project, index) => {
-    return <Project key={index} title={project.title} img={project.img} description={project.description} />
+    return <Project key={index} id={index} title={project.title} img={project.img} description={project.description} />
   })
-  
+
   return (
     <div>
       {show}
